f732e5cf786fd7c3f0fdb830ac947edabc874abb
Charles Latrobe <Charlie.bateman117@gmail.com>
2020-12-17 14:40:24 -0800

    add function to create forecast cards. remove extraneous text functions.

--- develop/script.js
+++ develop/script.js
@@ -1,32 +1,30 @@
-        const APIKey = "43b5b86a3681230a45961cc32737a765";
-        const citySearch = $('#citySearchID');
-        const savedCitiesSidebar = $('#savedCitiesSidebar');
-        let cityName = $("#cityName");
-        let tempSlot = $("#tempSlot");
-        let humiditySlot = $("#humiditySlot");
-        let windSpeedSlot = $("#windSpeedSlot");
-        let uvSlot = $('#uvSlot');
-        var searchedCities = [];
-        const forecastCardCell = $('#forecastCardCell');
+    const APIKey = "43b5b86a3681230a45961cc32737a765";
+    const citySearch = $('#citySearchID');
+    const savedCitiesSidebar = $('#savedCitiesSidebar');
+    let cityName = $("#cityName");
+    let tempSlot = $("#tempSlot");
+    let humiditySlot = $("#humiditySlot");
+    let windSpeedSlot = $("#windSpeedSlot");
+    let uvSlot = $('#uvSlot');
+    var searchedCities = [];
+    const forecastCardCell = $('#forecastCardCell');
 
-        const runWeather = function () {
-            getWeather((this).innerHTML);
-            getForecast((this).innerHTML);
-        };
+    const runWeather = function () {
+        getWeather((this).innerHTML);
+        getForecast((this).innerHTML);
+    };
 
-        getSavedCities();
+    getSavedCities();
 
-        $("#citySearchBtn").click(function (){
-        
-          const city = (citySearch).val();
-          
-          getWeather(city);
-          getForecast(city);
-        })
+    $("#citySearchBtn").click(function (){
+        const city = (citySearch).val();
+        getWeather(city);
+        getForecast(city);
+    })
 
-        function getWeather(city){
-          let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-          $.ajax({
+    function getWeather(city){
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+        $.ajax({
             url: queryURL,
             method: "GET"
             }).then(function(response) {
@@ -40,57 +38,51 @@
                 let name = response.name
                 getUV(lat, lon, name);
             })
-            .catch(function(err){console.log(err)});
-        }
+        .catch(function(err){console.log(err)});
+    }
 
-        function saveCity (city) {
-          if (searchedCities.indexOf(city) === -1) {
-                searchedCities.push(city);
-                makeCityBtn(city);
-                } else {
-                  console.log("already got 'im!")
-                  }
-          window.localStorage.setItem("cities", JSON.stringify(searchedCities))        
-        }
+    function saveCity (city) {
+        if (searchedCities.indexOf(city) === -1) {
+            searchedCities.push(city);
+            makeCityBtn(city);
+        } else {
+            console.log("already got 'im!")
+            }
+        window.localStorage.setItem("cities", JSON.stringify(searchedCities))        
+    }
 
-        function getSavedCities () {
-          let savedCities = JSON.parse(localStorage.getItem('cities')) || [];
-          for (let i = 0; i < savedCities.length; i++) {
+    function getSavedCities () {
+        let savedCities = JSON.parse(localStorage.getItem('cities')) || [];
+        for (let i = 0; i < savedCities.length; i++) {
             searchedCities.push(savedCities[i]);
             makeCityBtn(savedCities[i]);  
-          }
-        };
-        function test () {
-          console.log('hello');
-        }     
-        function makeCityBtn (city) {
-          $("<button>").text(city).addClass('success button cityBtn').on("click", runWeather).appendTo(savedCitiesSidebar)
         }
+    };
+  
+    function makeCityBtn (city) {
+        $("<button>").text(city).addClass('success button cityBtn').on("click", runWeather).appendTo(savedCitiesSidebar)
+    };
 
-        function getForecast (city) {
-          console.log(city);
-          let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=" + 5 + "&units=imperial&appid=" + APIKey;
-          $.ajax({
+    function getForecast (city) {
+        console.log(city);
+        let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=" + 5 + "&units=imperial&appid=" + APIKey;
+        $.ajax({
             url: queryURL,
             method: "GET"
             }).then(function(response) {
-              console.log(typeof(response.list));
-              Object.keys(response.list).forEach(key => {
-	              //console.log("key: ", key);
-                //console.log("Value: ", response.list[key]);
-                
-              });
-            })
-        }
+                createForecastCards(response.list);    
+        })
+    };
 
-        function getUV (lat, lon, name) {
-          let queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey
-          $.ajax({
+    function getUV (lat, lon, name) {
+        let queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey
+        $.ajax({
             url: queryURL,
             method: "GET"
             }).then(function(response) {
               uvSlot.text("UV Index: " + response.value);
-        })}
+        })
+    }
 
     let savedCities = JSON.parse(localStorage.getItem('cities')) || [];  
     if (savedCities.length) {
@@ -99,4 +91,12 @@
     } else {
       getWeather('Nashville');
       getForecast('Nashville');
+    }
+
+    function createForecastCards (data) {
+        Object.keys(data.list).forEach(key => {
+            console.log("key: ", key);
+            console.log("Value: ", data.list[key]);
+            
+          });
     }
